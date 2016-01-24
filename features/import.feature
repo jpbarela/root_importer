ca923f7d7df54590729330dcfee57ab3f7cd8a1a
J.P. Barela <barela@jpbarela.com>
2016-01-24 14:32:36 -0700

    Updated feature specs for displaying report information.

--- features/import.feature
+++ features/import.feature
@@ -3,5 +3,17 @@ Feature:
   In order to
 
   Scenario:
-    When I import a file
+    When I import the file 'sample.txt'
     Then I see a listing of the drivers
+
+  Scenario:
+    When I import the file 'sample.txt'
+    Then I see the trip statistics for each driver
+
+  Scenario:
+    When I import the file 'sample.txt'
+    Then I see the drivers sorted by distance
+
+  Scenario:
+    When I import the file 'excluded.txt'
+    Then I see invalid trips excluded
--- features/steps/import_steps.rb
+++ features/steps/import_steps.rb
@@ -1,8 +1,21 @@
-When(/^I import a file$/) do
-  @report = `ruby import_data.rb features/data/sample.txt`
+When(/^I import the file '([\w.]+)'$/) do |file|
+  @report = `ruby import_data.rb features/data/#{file}`
 end
 
 Then(/^I see a listing of the drivers$/) do
   drivers = @report.split("\n").map { |report_line| report_line[0, report_line.index(':')] }
   expect(drivers).to match_array %w(Alex Dan Ilya)
+end
+
+Then(/^I see the trip statistics for each driver$/) do
+  drivers = @report.split("\n")
+  expect(drivers).to match_array ['Alex: 42 miles @ 34 mph', 'Dan: 30 miles @ 36 mph', 'Ilya: 0 miles']
+end
+
+Then(/^I see the drivers sorted by distance$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see invalid trips excluded$/) do
+  pending # express the regexp above with the code you wish you had
 end
