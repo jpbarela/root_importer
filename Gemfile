d7ebc0212fb4eef809d224e672f4b0eb0c7fde58
J.P. Barela <barela@jpbarela.com>
2016-01-24 14:29:56 -0700

    Added rubocop support.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,11 @@
+source 'https://rubygems.org'
+
+group :development do
+  group 'rubocop'
+end
+
 group :test do
   gem 'cucumber'
-  gem 'pry'
-  gem 'pry-nav'
+  gem 'pry-byebug'
   gem 'rspec'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,13 +9,11 @@ RSpec.configure do |config|
 
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
-  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.example_status_persistence_file_path = 'spec/examples.txt'
   config.disable_monkey_patching!
   config.warnings = true
 
-  if config.files_to_run.one?
-    config.default_formatter = 'doc'
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.profile_examples = 10
 
